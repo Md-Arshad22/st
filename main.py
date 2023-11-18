de402e9e67ec9006c4acb1a8b67769746304f62e
Arshad <121594418+Md-Arshad22@users.noreply.github.com>
2023-11-19 00:42:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ df = pd.read_csv('cricket.csv')
 
 if st.sidebar.button('Click'):
   st.write(df)
-batters=('kl rahul, kishan')
-# options =st.sidebar.selectbox('choose option',('Batter':batters,'Allrounder','WicketKeeper','Bowlers'))
-# st.write(options)
-st.write(batters)
+WicketKeeper=['kl rahul', 'kishan']
+options =st.sidebar.selectbox('choose option',(['Batter','Allrounder','WicketKeeper','Bowlers']))
+st.write(options)
+# st.write(batters)
