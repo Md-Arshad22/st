5a779ade060d247ddef39a353c06bc95365b52bd
Arshad <121594418+Md-Arshad22@users.noreply.github.com>
2023-11-19 01:00:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ if st.sidebar.button('Click'):
   st.write(df)
 WicketKeeper=['kl rahul', 'kishan']
 kipper=', '.join(WicketKeeper)
-options =st.sidebar.selectbox('choose option',('batter','Allrounder','WicketKeeper','Bowlers'))
-st.write(options,kipper)
+# All=', '.join(
+options =st.sidebar.selectbox('choose option',(batter:'kipper','Allrounder','WicketKeeper','Bowlers'))
+st.write(options)
 
 
 
