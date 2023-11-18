5cd27030dc758c485eb525653853ec5f9ba19655
Arshad <121594418+Md-Arshad22@users.noreply.github.com>
2023-11-19 00:12:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ df = pd.read_csv('cricket.csv')
 if st.sidebar.button('Click'):
   st.write(df)
 options =['option1','option2','option3']
-selectoption =st.selectbox('select and option',options)
-
-st.sidebar.write(selectoption)
+selectoption =st.selectbox(options)
+if st.sidebar.button("select options"):
+  st.write(selectoption)
