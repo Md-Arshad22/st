dbb0e88eeadde3c0957970283c63e9ea7508e4a7
Arshad <121594418+Md-Arshad22@users.noreply.github.com>
2023-11-20 12:08:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,19 +9,43 @@ df = pd.read_csv('cricket.csv')
 if st.sidebar.button('Click'):
   st.write(df)
   
-WicketKeeper=['kl rahul', 'kishan']
-kipper=', '.join(WicketKeeper)
+# WicketKeeper=['kl rahul', 'kishan']
+# kipper=', '.join(WicketKeeper)
 
-Allrounder=['Arshad']
-All=', '.join(Allrounder)
-options =st.sidebar.selectbox('Batter','Wicketkeeper','Allrounder','Bowler')
-st.write(options,Batter)
+# Allrounder=['Arshad']
+# All=', '.join(Allrounder)
+# options =st.sidebar.selectbox('Batter','Wicketkeeper','Allrounder','Bowler')
+# st.write(options,Batter)
 
 df1 = pd.read_csv('Squads.csv')
 
 if st.sidebar.button('Squad'):
   st.write(df1)
-  
+
+import streamlit as st
+
+# Store the initial value of widgets in session state
+if "visibility" not in st.session_state:
+    st.session_state.visibility = "visible"
+    st.session_state.disabled = False
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.checkbox("Disable selectbox widget", key="disabled")
+    st.radio(
+        "Set selectbox label visibility 👉",
+        key="visibility",
+        options=["visible", "hidden", "collapsed"],
+    )
+
+with col2:
+    option = st.selectbox(
+        "How would you like to be contacted?",
+        ("Email", "Home phone", "Mobile phone"),
+        label_visibility=st.session_state.visibility,
+        disabled=st.session_state.disabled,
+    )
 
 
 
