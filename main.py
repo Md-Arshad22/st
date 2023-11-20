731a074e6c020d6a2fc040b68f7f41657ba15e46
Arshad <121594418+Md-Arshad22@users.noreply.github.com>
2023-11-20 11:59:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,6 +23,12 @@ if st.sidebar.button('Squad'):
 
 
 
+option = st.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone'))
+
+st.write('You selected:', option)
+
 
 
 
