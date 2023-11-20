a7137cc4e1e327e0dafe7faaab3c694afaf44115
Arshad <121594418+Md-Arshad22@users.noreply.github.com>
2023-11-20 14:20:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,24 +8,13 @@ df = pd.read_csv('cricket.csv')
 
 if st.sidebar.button('Click'):
   st.write(df)
-  
-# WicketKeeper=['kl rahul', 'kishan']
-# kipper=', '.join(WicketKeeper)
-
-# Allrounder=['Arshad']
-# All=', '.join(Allrounder)
-# options =st.sidebar.selectbox('Batter','Wicketkeeper','Allrounder','Bowler')
-# st.write(options)
-#st.write(kipper)
-#st.text(kipper)
 
 df1 = pd.read_csv('Squads.csv')
 
 if st.sidebar.button('Squad'):
   st.write(df1)
 
-
-option = st.selectbox(
+option = st.sidebar.selectbox(
     'How would you like to be contacted?',
     ('Email', 'Home phone', 'Mobile phone'))
 
