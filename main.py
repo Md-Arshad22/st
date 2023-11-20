2ae5df9b87ea100220bdefb804b02773338de6d1
Arshad <121594418+Md-Arshad22@users.noreply.github.com>
2023-11-20 15:06:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,18 +10,10 @@ df
 #   st.write(df.describe())
 
 df1 = pd.read_csv('Squads.csv')
-st.pyplot(df1)
+
 if st.sidebar.button('Squad'):
   st.write(df1)
 
-# arr = np.random.normal(1, 1,size=100)
-# fig, ax = plt.subplots()
-# ax.hist(arr, bins=20)
-# st.pyplot(fig)
-
-# a1 = pd.DataFrame(df['year'],df['Total Price'])
-# st.line_chart(a1)
-# st.pyplot(fig)
 
 option = st.sidebar.selectbox(
     'Select!',
