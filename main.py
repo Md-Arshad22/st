54a3bedf9ab251ecb74c3c2638407ad14079c5f8
Arshad <121594418+Md-Arshad22@users.noreply.github.com>
2023-11-20 16:13:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ if st.sidebar.button('Click Squad'):
 option = st.sidebar.selectbox(
     'Select!',
     ('Batter','WicketKeeper', 'All-Rounder','Bowler')
-df2 = pd.read_csv('Squads.csv')
 )
 st.write('Selected:', option)
 
