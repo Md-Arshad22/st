a5bfbffd00c6b864263e6cb507ae40eee7571ad3
Arshad <121594418+Md-Arshad22@users.noreply.github.com>
2023-11-21 11:16:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import streamlit as st
 
 df = pd.read_csv('cricket.csv')
 st.title('ICC ODI MENS CRICKET WORLD CUP 2023')
-st.subheader('POINTS TABLE WORLD CUP 2023')\
+st.subheader('POINTS TABLE WORLD CUP 2023')
 df
 
 # df1 = pd.read_csv('Squads.csv')
