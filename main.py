c0db2f12b2ab0ec92efc7afaa960d0e9e2d729bf
Arshad <121594418+Md-Arshad22@users.noreply.github.com>
2023-11-21 11:12:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-# import seaborn as sns
+import seaborn as sns
 import streamlit as st
 
 st.title('ICC ODI MENS CRICKET WORLD CUP 2023')
 st.subheader('POINT TABLE WORLD CUP2023')
 df = pd.read_csv('cricket.csv')
-plt.bar(df)
-plt.show()
 
 df1 = pd.read_csv('Squads.csv')
 if st.sidebar.button('Click Squad'):
