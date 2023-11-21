319c45c854eadcdf1d82cff39f0e5cf9da50cafc
Arshad <121594418+Md-Arshad22@users.noreply.github.com>
2023-11-21 10:07:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -33,8 +33,11 @@ genre = st.radio(
 
 if genre == ':Australia':
     st.write('You selected Australia.')
+
+if genre == ':South Africa":
+    st.write('You Selected South Africa')
 else:
-    st.write("You didn\'t select Australia.")
+    st.write("You didn\'t select Any Champion Team.")
 
 
 agree = st.checkbox('I agree')
