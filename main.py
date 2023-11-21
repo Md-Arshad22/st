e54488735f1093ab10638923ca8b46b702bc2fb3
Arshad <121594418+Md-Arshad22@users.noreply.github.com>
2023-11-21 10:15:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -26,6 +26,12 @@ if st.sidebar.button('Click Bowler'):
 # st.write('Selected:', option)
 
 
+genre = st.radio(
+    "What's your favorite movie genre",
+    [":rainbow[Comedy]", "***Drama***", "Documentary :movie_camera:"],
+    index=None,
+)
+st.write("You selected:", genre)
 
 
 
