f736c588b5d4291eeee775e285b7f84a618dc476
Arshad <121594418+Md-Arshad22@users.noreply.github.com>
2023-11-21 16:51:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,9 +13,10 @@ df
 df1 = pd.read_csv('Squads.csv')
 if st.sidebar.button('Click Squad'):
     st.write(df1)
-# df1 = pd.read_csv('Squads.csv')
-# if st.sidebar.button('Click Squad'):
-#     st.write(df1)
+
+df2 pd.read_csv('Bowler.csv')
+if st.sidebar.button('Click Bowler'):
+    st.write(df1)
     
 # df2 = pd.read_csv('Bowler.csv')
 # if st.sidebar.button('Click Bowler'):
