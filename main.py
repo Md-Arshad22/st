e3efb45a8a1a19f08e6eb816e8fc577b11123d5f
Arshad <121594418+Md-Arshad22@users.noreply.github.com>
2023-11-22 10:32:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ explode = (0, 0.1, 0, 0)
 fig1, ax1 = plt.subplots()
 ax1.pie(sizes, explode= explode, labels = labels, autopct='%1.1f%',
         shadow= True, startangle=90)
-ax1.axis('eqaul')
+ax1.axis('equal')
 st.pyplot(fig1)
 
 
