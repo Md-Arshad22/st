4ded98899af0516314acc5219b37e68d2bb8797e
Arshad <121594418+Md-Arshad22@users.noreply.github.com>
2023-11-22 10:36:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -26,13 +26,13 @@ df4 = pd.read_csv('batter.csv')
 if st.sidebar.button('Batter'):
     st.write(df4)
 
-# labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
-# sizes = [15,30,45,10]
-# explode = (0, 0.1, 0, 0)
-# fig1, ax1 = plt.subplots()
-# ax1.pie(sizes, explode= explode, labels = labels, autopct='%1.1f%',
-#         shadow= True, startangle=90)
-# ax1.axis('equal')
-# st.pyplot(fig1)
+labels = 'Batter', 'WicketKeeper', 'All-rounder', 'Bowler'
+sizes = [15,30,45,10]
+explode = (0, 0.1, 0, 0)
+fig1, ax1 = plt.subplots()
+ax1.pie(sizes, explode= explode, labels = labels, autopct='%1.1f%',
+        shadow= True, startangle=90)
+ax1.axis('equal')
+st.pyplot(fig1)
 
 
