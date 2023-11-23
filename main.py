316ebb007a54cfbadea1f02c4d5d6662893b3b1e
Arshad <121594418+Md-Arshad22@users.noreply.github.com>
2023-11-23 15:55:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,39 +8,8 @@ st.title('ICC ODI MENS CRICKET WORLD CUP 2023')
 st.subheader('POINT TABLE WORLD CUP 2023')
 
 
-# st.title('ICC ODI MENS CRICKET WORLD CUP 2023')
-# st.subheader('POINTS TABLE WORLD CUP 2023')
-# df = pd.read_csv('cricket.csv')
-# df
-
-# if st.sidebar.button('load description'):
-#     st.write(df.describe())
-    
-# df1 = pd.read_csv('Squads.csv')
-# if st.sidebar.button('Click Squad'):
-#     st.write(df1)
 
 
-# df2 = pd.read_csv('Bowler.csv')
-# if st.sidebar.button('Click Bowler'):
-#     st.write(df2)
-    
-# df3 = pd.read_csv('All.csv')
-# if st.sidebar.button('All'):
-#     st.write(df3)
-
-# df4 = pd.read_csv('batter.csv')
-# if st.sidebar.button('Batter'):
-#     st.write(df4)
-
-# # x = np.array(['SNO','TEAM','M','W','L','NRR','Pts','Last 5'])
-# # y = np.array([17,10,15,50,15,28,30,45])
-# # plt.bar(x,y)
-# # plt.show()
-# x = [20,30,50]
-# y = [100,200,500]
-# plt.bar(x,y)
-# plt.show()
 
 
 
