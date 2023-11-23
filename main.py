fa0a019a87918188752b53d515f838f121bd09ae
Arshad <121594418+Md-Arshad22@users.noreply.github.com>
2023-11-23 12:29:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,32 +4,34 @@ import matplotlib.pyplot as plt
 # import seaborn as sns
 import streamlit as st
 
-
-
 st.title('ICC ODI MENS CRICKET WORLD CUP 2023')
-st.subheader('POINTS TABLE WORLD CUP 2023')
-df = pd.read_csv('cricket.csv')
-df
+st.subheader('POINT TABLE WORLD CUP 2023')
+
+
+# st.title('ICC ODI MENS CRICKET WORLD CUP 2023')
+# st.subheader('POINTS TABLE WORLD CUP 2023')
+# df = pd.read_csv('cricket.csv')
+# df
 
 # if st.sidebar.button('load description'):
 #     st.write(df.describe())
     
-df1 = pd.read_csv('Squads.csv')
-if st.sidebar.button('Click Squad'):
-    st.write(df1)
+# df1 = pd.read_csv('Squads.csv')
+# if st.sidebar.button('Click Squad'):
+#     st.write(df1)
 
 
-df2 = pd.read_csv('Bowler.csv')
-if st.sidebar.button('Click Bowler'):
-    st.write(df2)
+# df2 = pd.read_csv('Bowler.csv')
+# if st.sidebar.button('Click Bowler'):
+#     st.write(df2)
     
-df3 = pd.read_csv('All.csv')
-if st.sidebar.button('All'):
-    st.write(df3)
+# df3 = pd.read_csv('All.csv')
+# if st.sidebar.button('All'):
+#     st.write(df3)
 
-df4 = pd.read_csv('batter.csv')
-if st.sidebar.button('Batter'):
-    st.write(df4)
+# df4 = pd.read_csv('batter.csv')
+# if st.sidebar.button('Batter'):
+#     st.write(df4)
 
 # # x = np.array(['SNO','TEAM','M','W','L','NRR','Pts','Last 5'])
 # # y = np.array([17,10,15,50,15,28,30,45])
